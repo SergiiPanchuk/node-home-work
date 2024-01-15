7e0a4019648dfc007ac64db2164af2cd48dbf649
Сергій <sergiip1994@gmail.com>
2024-01-15 22:36:00 +0200

    add lessons 2

---
+++ src/fs.service.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+import { IUser } from "./interfaces/user.interface";
+
+const pathToFile = path.join(process.cwd(), "db.json");
+
+const read = async (): Promise<IUser[]> => {
+    const json = await fs.readFile(pathToFile, { encoding: "utf-8" });
+    return JSON.parse(json);
+};
+
+const write = async (users: IUser[]): Promise<void> => {
+    await fs.writeFile(pathToFile, JSON.stringify(users));
+};
+
+export { read, write };
