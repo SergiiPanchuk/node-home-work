a59e6b95cb23dc479d26972afcba70e5c91856ed
Сергій <sergiip1994@gmail.com>
2024-01-17 22:59:35 +0200

    add lessons 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    extends: [
+        "plugin:prettier/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "simple-import-sort"],
     root: true,
+    rules: {
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+    },
 };
